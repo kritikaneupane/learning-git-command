b84b0b56d01b17c5eb59bb7f1bbe4060cf33074e
Your Name <neupanekritika49@gmail.com>
2023-12-24 15:06:11 +0545

    switchcase

--- learning.js
+++ learning.js
@@ -1,6 +1,29 @@
-let a = 10;
-let b = 20;
-let c = a;
-a + b;
-b = c;
-console.log("value of a", a, "value of b", b);
+let a = prompt("please enter any day");
+switch (a) {
+  case "sunday":
+    alert("today you will have orange as lunch");
+    break;
+}
+//case"monday":
+alert("today you will have tomato as lunch");
+//break;
+//case"tuesday":
+alert("today you will have vaat as lunch");
+//break;
+//case"wednesday":
+alert("today you will have dal as lunch");
+//break;
+
+//case"thrusday":
+alert("today you will have apple as lunch");
+//break;
+
+//case"friday":
+alert("today you will have aalu as lunch");
+//break;
+
+//case"saturday":
+alert("today you will have coke as lunch");
+// break;
+//default:
+alert(" please input correct days");
